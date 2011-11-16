a2ee4b5b6998f190c836bb35472d7496ec57f5c3
bgrins <briangrinstead@gmail.com>
2011-11-16 16:34:33 -0600

    whitespace

--- app.js
+++ app.js
@@ -5,23 +5,20 @@ function init() {
         move: colorpickerMove
     });
 
-
     console.log(1, 2, 3, ["This is an array", document.body]);
     console.log({
         "strong": ["b", "strong", "big"],
         "emphasized": ["em", "i"]
     });
     
-    
     console.time("random timeout");
     setTimeout(function() {
         console.timeEnd("random timeout")
-    }, Math.random() * 1000)
-
+    }, Math.random() * 1000);
 }
 
-function colorpickerMove(tinycolor) {
-    console.log(tinycolor);    
+function colorpickerMove(color) {
+    console.log("colorpicker moved", color);
 }
 
 function setColorpicker(color) {
