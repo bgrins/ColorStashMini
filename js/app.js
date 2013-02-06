a840d2c44f6781b9bc1ec2aaed9a14a7e1426012
bgrins <briangrinstead@gmail.com>
2013-02-05 18:14:48 -0600

    more updates

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 
 function init() {
+
     $("#spec").spectrum({
         flat:true,
-        move: colorpickerMove
+        showInput: true,
+        preferredFormat: "rgb",
+        move: colorpickerMove,
+        color: "orange"
     });
 
     console.log(1, 2, 3, ["This is an array", document.body]);
@@ -10,15 +14,31 @@ function init() {
         "strong": ["b", "strong", "big"],
         "emphasized": ["em", "i"]
     });
-    
+
     console.time("random timeout");
     setTimeout(function() {
-        console.timeEnd("random timeout")
+        console.timeEnd("random timeout");
     }, Math.random() * 1000);
 }
 
 function colorpickerMove(color) {
     console.log("colorpicker moved", color);
+
+    $(".palette-container").empty();
+
+    drawPalette(tinycolor.analogous(color));
+
+}
+
+function drawPalette(colors) {
+    var container = $("<ul />").appendTo(".palette-container");
+    var html = [];
+    for (var i = 0; i < colors.length; i++) {
+        var hex = colors[i].toHexString();
+        html.push("<li style='background-color:" + hex + ";'></li>");
+    }
+
+    container.html(html.join(""));
 }
 
 function setColorpicker(color) {
